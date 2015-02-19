d7041cf7de087de0ecaf2e433c255f1780c64812
jmfryan <jryan59@googlemail.com>
2015-02-19 08:17:13 +0000

    fix robots check

--- validators/robots.js
+++ validators/robots.js
@@ -2,16 +2,15 @@ var robots = require('robots');
 var url = require('url');
 var parser = new robots.RobotsParser();
 
+var robots =  "https://www.movebubble.com/robots.txt";
+parser.setUrl(robots);
+
 module.exports = function Robots(error, result, $, report) {
  	var uri = url.parse(result.uri);
 
- 	var robots = uri.protocol + "//" + uri.host + "/robots.txt";
-
-    parser.setUrl(robots);
-
-    if(!parser.canFetchSync('', result.url.pathname)) {
+    if(!parser.canFetchSync('', uri.pathname)) {
     	report.addMessage("INFO", "blocked from indexing by robots.txt");
-    	reoport.setIndexable(false);
+    	report.setIndexable(false);
     } 
 
     var metaRobots = $('meta[name=robots]');
